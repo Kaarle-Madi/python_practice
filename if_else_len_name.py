ead99e473246a9f925ed462665151d528c233c25
Suramethar Chantarasmee <76833027+Kaarle-Madi@users.noreply.github.com>
2022-10-27 16:14:08 +0700

    Add files via upload

---
+++ if_else_len_name.py
@@ -0,0 +1,10 @@
+name = "madi"
+
+if len(name) < 3:
+    print("name must be more than 3 characters")
+
+elif len(name) > 50:
+    print("maximum characters is 50")
+
+else:
+    print("Your name look really good")
